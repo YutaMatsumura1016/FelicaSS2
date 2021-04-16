f70e3e994725944baa4accbc09cee63a9c6889ec
YutaMatsumura1016 <yuta.train1016@gmail.com>
2021-04-16 13:41:30 +0900

    ボタンをXMLに移植する直前

--- app/src/main/java/com/example/felicass2/MainActivity.java
+++ app/src/main/java/com/example/felicass2/MainActivity.java
@@ -53,37 +53,20 @@ import java.util.List;
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
-class Data{
-    public String name;
-    public int age;
-}
-
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
     //SS
-    GoogleAccountCredential mCredential;
     private TextView mOutputText;
     private Button mCallApiButton;
-    ProgressDialog mProgress;
-
-    static final int REQUEST_ACCOUNT_PICKER = 1000;
-    static final int REQUEST_AUTHORIZATION = 1001;
-    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
-    static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
-
     private static final String BUTTON_TEXT = "読み取り開始";
     private static final String BUTTON_TEXT2 = "読み取り中";
-    private static final String PREF_ACCOUNT_NAME = "accountName";
-    private static final String[] SCOPES = {SheetsScopes.SPREADSHEETS};
 
 
     //Felica
     SoundPool soundPool;
     int soundpi;
     NfcAdapter nfcAdapter;
-    static String idmString10 = "エラー：初期値です。コードを確認してください。";
     static int i = 1;
-
 
 
     //スーパークラス両方分
@@ -113,10 +96,7 @@ public class MainActivity extends AppCompatActivity  {
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
 
-
         class MyReaderCallback implements NfcAdapter.ReaderCallback {
-
-            String idmString2;
 
             @RequiresApi(api = Build.VERSION_CODES.N)
 //            @Override
@@ -127,14 +107,12 @@ public class MainActivity extends AppCompatActivity  {
 
                 //16進数に変換
                 StringBuilder sb = new StringBuilder();
-                for (byte d: idm) {
+                for (byte d : idm) {
                     sb.append(String.format("%02X", d));
                 }
                 String idmString = sb.toString();
                 Log.d("hoge", idmString);
                 MyReaderCallback mr = new MyReaderCallback();
-                mr.idmString2 = idmString;
-                idmString10 = mr.idmString2;
 
 
                 //音を鳴らしてSSに送る
@@ -143,25 +121,20 @@ public class MainActivity extends AppCompatActivity  {
                     @Override
                     public void run() {
                         setContentView(R.layout.activity_main);
-                        WebView  myWebView = (WebView)findViewById(R.id.webView1);
-//                        myWebView.setVisibility(View.GONE);
+                        WebView myWebView = (WebView) findViewById(R.id.webView1);
                         //標準ブラウザを起動させない
                         myWebView.setWebViewClient(new WebViewClient());
-                        myWebView.loadUrl("https://script.google.com/a/wasedasai.net/macros/s/AKfycbw9BMWL3BLRhB8ZlIs32scTBWceP0TYy28wnWtBD2btOatmNiiw/exec?name=" + idmString10);
+                        String sentURL = "https://script.google.com/a/wasedasai.net/macros/s/AKfycbw9BMWL3BLRhB8ZlIs32scTBWceP0TYy28wnWtBD2btOatmNiiw/exec?idm=" + idmString;
+                        myWebView.loadUrl(sentURL);
+                        Log.d("huga", sentURL);
 
                         soundPool.play(soundpi, 1.0f, 1.0f, 0, 0, 1);
-                        //SSに送信
-                        getResultsFromApi();
-                        i++;
                     }
                 });
 
             }
 
         }
-
-
-
 
 
         //------------------元SSAPIのスーパークラス---------------------
@@ -199,272 +172,14 @@ public class MainActivity extends AppCompatActivity  {
         mOutputText.setVerticalScrollBarEnabled(true);
         mOutputText.setMovementMethod(new ScrollingMovementMethod());
         mOutputText.setText(
-                "「" + BUTTON_TEXT +"」ボタンを押して読み取りを始める");
+                "「" + BUTTON_TEXT + "」ボタンを押して読み取りを始める");
         activityLayout.addView(mOutputText);
-
-        mProgress = new ProgressDialog(this);
-        mProgress.setMessage("Google Sheets APIに接続中");
-
         setContentView(activityLayout);
 
-        // Initialize credentials and service object.
-        mCredential = GoogleAccountCredential.usingOAuth2(
-                getApplicationContext(), Arrays.asList(SCOPES))
-                .setBackOff(new ExponentialBackOff());
 
     }//スーパークラス終わり
-
-
-
-
-//-----------------SSの継承クラス----------------------------
-
-
-    //ボタン押した後どうするか
-    private void getResultsFromApi() {
-        if (! isGooglePlayServicesAvailable()) {
-            acquireGooglePlayServices();
-        } else if (mCredential.getSelectedAccountName() == null) {
-            chooseAccount();
-        } else if (! isDeviceOnline()) {
-            mOutputText.setText("インターネット接続がありません");
-        } else {
-            new MakeRequestTask(mCredential).execute();
-        }
-    }
-
-
-    @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
-    private void chooseAccount() {
-        if (EasyPermissions.hasPermissions(
-                this, Manifest.permission.GET_ACCOUNTS)) {
-            String accountName = getPreferences(Context.MODE_PRIVATE)
-                    .getString(PREF_ACCOUNT_NAME, null);
-            if (accountName != null) {
-                mCredential.setSelectedAccountName(accountName);
-                getResultsFromApi();
-            } else {
-                // Start a dialog from which the user can choose an account
-                startActivityForResult(
-                        mCredential.newChooseAccountIntent(),
-                        REQUEST_ACCOUNT_PICKER);
-            }
-        } else {
-            // Request the GET_ACCOUNTS permission via a user dialog
-            EasyPermissions.requestPermissions(
-                    this,
-                    "このアプリは「連絡先」へのアクセスを必要とします。",
-                    REQUEST_PERMISSION_GET_ACCOUNTS,
-                    Manifest.permission.GET_ACCOUNTS);
-        }
-    }
-
-
-    @Override
-    protected void onActivityResult(
-            int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch(requestCode) {
-            case REQUEST_GOOGLE_PLAY_SERVICES:
-                if (resultCode != RESULT_OK) {
-                    mOutputText.setText(
-                            "このアプリはPlayStoreを必要とします。アプリを再起動してください。");
-                } else {
-                    getResultsFromApi();
-                }
-                break;
-            case REQUEST_ACCOUNT_PICKER:
-                if (resultCode == RESULT_OK && data != null &&
-                        data.getExtras() != null) {
-                    String accountName =
-                            data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-                    if (accountName != null) {
-                        SharedPreferences settings =
-                                getPreferences(Context.MODE_PRIVATE);
-                        SharedPreferences.Editor editor = settings.edit();
-                        editor.putString(PREF_ACCOUNT_NAME, accountName);
-                        editor.apply();
-                        mCredential.setSelectedAccountName(accountName);
-                        getResultsFromApi();
-                    }
-                }
-                break;
-            case REQUEST_AUTHORIZATION:
-                if (resultCode == RESULT_OK) {
-                    getResultsFromApi();
-                }
-                break;
-        }
-    }
-
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        EasyPermissions.onRequestPermissionsResult(
-                requestCode, permissions, grantResults, this);
-    }
-
-
-
-    public void onPermissionsGranted(int requestCode, List<String> list) {
-        // Do nothing.
-    }
-
-
-
-    //    @Override
-    public void onPermissionsDenied(int requestCode, List<String> list) {
-        // Do nothing.
-    }
-
-
-
-    private boolean isDeviceOnline() {
-        ConnectivityManager connMgr =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        return (networkInfo != null && networkInfo.isConnected());
-    }
-
-
-    private boolean isGooglePlayServicesAvailable() {
-        GoogleApiAvailability apiAvailability =
-                GoogleApiAvailability.getInstance();
-        final int connectionStatusCode =
-                apiAvailability.isGooglePlayServicesAvailable(this);
-        return connectionStatusCode == ConnectionResult.SUCCESS;
-    }
-
-
-    private void acquireGooglePlayServices() {
-        GoogleApiAvailability apiAvailability =
-                GoogleApiAvailability.getInstance();
-        final int connectionStatusCode =
-                apiAvailability.isGooglePlayServicesAvailable(this);
-        if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
-            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
-        }
-    }
-
-
-    void showGooglePlayServicesAvailabilityErrorDialog(
-            final int connectionStatusCode) {
-        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
-        Dialog dialog = apiAvailability.getErrorDialog(
-                MainActivity.this,
-                connectionStatusCode,
-                REQUEST_GOOGLE_PLAY_SERVICES);
-        dialog.show();
-    }
-
-
-
-    private class MakeRequestTask extends AsyncTask<Void, Void, List<String>>{
-        private com.google.api.services.sheets.v4.Sheets mService = null;
-        private Exception mLastError = null;
-
-        MakeRequestTask(GoogleAccountCredential credential) {
-            HttpTransport transport = AndroidHttp.newCompatibleTransport();
-            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-            mService = new com.google.api.services.sheets.v4.Sheets.Builder(
-                    transport, jsonFactory, credential)
-                    .setApplicationName("Google Sheets API Android Quickstart")
-                    .build();
-        }
-
-
-        @Override
-        protected List<String> doInBackground(Void... params) {
-            try {
-                putDataFromApi();
-                return getDataFromApi();
-            } catch (Exception e) {
-                mLastError = e;
-                cancel(true);
-                return null;
-            }
-        }
-
-        //データを書き込むメソッド
-        public void putDataFromApi() throws IOException {
-            String spreadsheetId = "1MXd7VHuOGZDwVmCjQbJk5np87-SLz-dkp3IA0YsGvMs";
-            String str = "A" + i;
-            String range = "data1!"+str;//データはとりあえずIDmだけ
-            ValueRange valueRange = new ValueRange();
-            List row = new ArrayList<>();
-            List col = Arrays.asList(idmString10);
-            row.add(col);
-            valueRange.setValues(row);
-            valueRange.setRange(range);
-            this.mService.spreadsheets().values()
-                    .update(spreadsheetId, range, valueRange)
-                    .setValueInputOption("USER_ENTERED")
-                    .execute();
-        }
-
-
-        private List<String> getDataFromApi() throws IOException {
-            String spreadsheetId = "1MXd7VHuOGZDwVmCjQbJk5np87-SLz-dkp3IA0YsGvMs";
-            String range = "data1!A1:D1";
-            List<String> results = new ArrayList<String>();
-            ValueRange response = this.mService.spreadsheets().values()
-                    .get(spreadsheetId, range)
-                    .execute();
-            List<List<Object>> values = response.getValues();
-            if (values != null) {
-                for (List row : values) {
-                    results.add(
-                            row.get(0) + ", " +
-                                    row.get(1) + ", " +
-                                    row.get(2) + ", " +
-                                    row.get(3)
-                    );
-                }
-            }
-            return results;
-        }
-
-
-
-        @Override
-        protected void onPreExecute() {
-            mOutputText.setText("");
-            mProgress.show();
-        }
-
-        @Override
-        protected void onPostExecute(List<String> output) {
-            mProgress.hide();
-            if (output == null || output.size() == 0) {
-                mOutputText.setText("通信に失敗しました。環境を確認してください。");
-            } else {
-                output.add(0, "通信に成功しました。");
-                mOutputText.setText(TextUtils.join("\n", output));
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mProgress.hide();
-            if (mLastError != null) {
-                if (mLastError instanceof GooglePlayServicesAvailabilityIOException) {
-                    showGooglePlayServicesAvailabilityErrorDialog(
-                            ((GooglePlayServicesAvailabilityIOException) mLastError)
-                                    .getConnectionStatusCode());
-                } else if (mLastError instanceof UserRecoverableAuthIOException) {
-                    startActivityForResult(
-                            ((UserRecoverableAuthIOException) mLastError).getIntent(),
-                            MainActivity.REQUEST_AUTHORIZATION);
-                } else {
-                    mOutputText.setText("エラーが発生しました:\n" + mLastError.getMessage());
-                }
-            } else {
-                mOutputText.setText("リクエストを中止しました。");
-            }
-        }
-    }
 }
+
+
+
+
